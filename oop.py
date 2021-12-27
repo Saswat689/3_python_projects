508b8a86231eb76a622300a68e03e9bac132a156
Saswat Singh <saswatsingh629@gmail.com>
2021-12-27 09:54:44 +0530

    added class templates

--- oop.py
+++ oop.py
@@ -1,5 +1,14 @@
+# OOP Principles
+# Here are some things I learnt n this project.
+
+# Encapsulation - 	Prevent user from accessing certain attributes.
+# Abstraction - Abstract(hide) the information that is unnecessary by using single _ before methods.
+# Inheritance - Inherit attributes from a parent class.
+# Polymorphism - A single entity that knows how to handle different objects.eg len() func in python.
+
 import csv
 
+# parent class
 class Item:
 
 	# class attribute
@@ -11,18 +20,29 @@ class Item:
 
 		# Run validations to the received args
 
-		assert price >= 0, f"Price {price} is not greator than or equal to zero!"
+		assert price >= 0, f"{self.__class__.__name__} {price} is not greator than or equal to zero!"
 		assert quantity >= 0, f"Quantity {price} is not greator than or equal to zero!"
 
 
 		# Assign to self object
-		self.name = name
+
+		self.__name = name
 		self.price = price
 		self.quantity = quantity
 
 		# Keep track of all of our instances
 		Item.all.append(self)
 
+	# Property Decorator = Read Only Attribute	
+	@property
+	def name(self):
+		return self.__name
+		
+
+	#allow users to set new values to attributes
+	@name.setter
+	def name(self,value):
+		self.__name = value	
 
 	# instance attribute
 	def calculate_total_price(self):
@@ -32,7 +52,8 @@ class Item:
 	def apply_discount(self):
 		self.price = self.price * self.pay_rate
 
-	# class method to read data
+
+	# class methods send class as a parameter
 	@classmethod
 
 	# instantiate the class via the data inside files(good practise)
@@ -51,13 +72,38 @@ class Item:
 			)
 
 
+	@staticmethod	
+	# static method doesn't receive anything	
+	def is_integer(num):
+		if isinstance(num,float):
+			return num.is_integer()
+		elif isinstance(num,int):
+			return True	
+		else:
+			return False	
+
+
 	# represent each instance (readable)
 	def __repr__(self):
 			return f"Item('{self.name}','{self.quantity}','{self.price}')"
+			
+
+# child class
+class Phone(Item):
+
+	def __init__(self,name: str,quantity=0,price=3,broken_phones=0):
+		# Call to super function to have access to all attributes/methods from the parent class
+		super().__init__(
+			name,price,quantity
+		)
+
+		# Run validations to the received args
+
+		assert broken_phones >= 0, f"Quantity {broken_phones} is not greator than or equal to zero!"
 
 
-Item.instantiate_from_csv()		
-print(Item.all)
+		# Assign to self object
+		self.broken_phones = broken_phones
 
 
 
